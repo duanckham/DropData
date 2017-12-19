8b96ea1771a2362f147a2eaacccfe89b8c4bce27
duanckham <duanckham@gmail.com>
2017-12-19 18:54:05 +0800

    MISC

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
-const Dropbox = require('dropbox');
+const moment = require('moment');
 const low = require('lowdb');
+const Dropbox = require('dropbox');
 const FileSync = require('lowdb/adapters/FileSync');
 
 class DropData {
@@ -9,31 +10,130 @@ class DropData {
     // this.db = low(this.adapter);
 
     this._options = options;
-    this._filename = `${this._options.name}.json`;
-
     this._dbx = new Dropbox({
       accessToken: options.accessToken
     });
 
-    this.syncFromRemote(this._options.path, this._options.name);
+    this.looper();
+  }
+
+  async sync(path, name) {
+    let localeFilePath = `${path}/${name}.json`;
+    let localeVersionPath = `${path}/${name}.version`;
+
+    let localeFileExisted = false;
+    let remoteFileExisted = false;
+
+    let localeFileVersion = 0;
+    let remoteFileVersion = 0;
+
+    let filesListFolderRes = await this._dbx.filesListFolder({ path: '' });
+
+    // Read locale file.
+    if (fs.existsSync(localeFilePath)) {
+      localeFileExisted = true;
+    }
+
+    // Read remote file.
+    for (let i = filesListFolderRes.entries.length; i--;) {
+      if (filesListFolderRes.entries[i].name === this._options.name + '.json') {
+        let _versionDownloadRes = await this._dbx.filesDownload({ path: `/${this._options.name}.version` });
+
+        remoteFileExisted = true;
+        remoteFileVersion = parseInt(_versionDownloadRes.fileBinary);
+      }
+    }
+
+    if (this._options.debug) {
+      console.log(`* localeFileExisted: ${localeFileExisted ? 'yes' : 'no'}`);
+      console.log(`* remoteFileExisted: ${remoteFileExisted ? 'yes' : 'no'}`);
+    }
+
+    if (localeFileExisted) {
+      if (remoteFileExisted) {
+        // Compare.
+        localeFileVersion = parseInt(fs.readFileSync(localeVersionPath));
+
+        if (this._options.debug) {
+          console.log(`* localeFileVersion: ${localeFileVersion}`);
+          console.log(`* remoteFileVersion: ${remoteFileVersion}`);
+        }
+
+        if (localeFileVersion > remoteFileVersion) {
+          await this.syncToRemote(path, name);
+        } else if (localeFileVersion < remoteFileVersion) {
+          await this.syncFromRemote(path, name);
+        }
+      } else {
+        // Upload to remote.
+        await this.syncToRemote(path, name);
+      }
+    } else {
+      if (remoteFileExisted) {
+        // Download to locale.
+        await this.syncFromRemote(path, name);
+      } else {
+        // Create locale file and upload to remote.
+        await this.initDataFile(path, name);
+      }
+    }
   }
 
   async syncFromRemote(path, name) {
-    let localFilePath = `${path}/${name}.json`;
-    let remoteFilePath = `${path}/${name}.remote.json`;
-    let filesListFolderRes = await this._dbx.filesListFolder({ path: '' });
+    // Download to locale.
+    let _fileDownloadRes = await this._dbx.filesDownload({ path: `/${this._options.name}.json` });
+    let _versionDownloadRes = await this._dbx.filesDownload({ path: `/${this._options.name}.version` });
 
-    filesListFolderRes.entries.forEach(async item => {
-      if (item.name === this._filename) {
-        let filesDownloadRes = await this._dbx.filesDownload({ path: '/' + this._filename });
+    // Yes, yes, yes.
+    fs.writeFileSync(`/${path}/${name}.json`, _fileDownloadRes.fileBinary);
+    fs.writeFileSync(`/${path}/${name}.version`, _versionDownloadRes.fileBinary);
+  }
 
-        fs.writeFileSync(remoteFilePath, filesDownloadRes.fileBinary);
+  async syncToRemote(path, name) {
+    // Delete remote files.
+    try {
+      await this._dbx.filesDeleteBatch({
+        entries: [
+          { path: `/${name}.json` },
+          { path: `/${name}.version` }
+        ]
+      });
+    } catch (error) {
+      console.log(error);
+    }
 
-        console.log(filesDownloadRes.fileBinary);
-      }
+    // Upload.
+    await this._dbx.filesUpload({
+      path: `/${name}.json`,
+      contents: fs.readFileSync(`${path}/${name}.json`)
     });
 
-    console.log(filesListFolderRes);
+    await this._dbx.filesUpload({
+      path: `/${name}.version`,
+      contents: fs.readFileSync(`${path}/${name}.version`)
+    });
+  }
+
+  async initDataFile(path, name) {
+    let localeFilePath = `${path}/${name}.json`;
+    let localeVersionPath = `${path}/${name}.version`;
+
+    // Create locale file.
+    fs.writeFileSync(localeFilePath, JSON.stringify({}));
+    fs.writeFileSync(localeVersionPath, 1);
+
+    // Upload to remote.
+    await this.syncToRemote(path, name);
+  }
+
+  looper() {
+    let _processer = () => {
+      this.sync(this._options.path, this._options.name);
+    };
+
+    setInterval(_processer.bind(this), 20000);
+
+    _processer();
   }
 }
 
@@ -42,5 +142,6 @@ module.exports = DropData;
 let dd = new DropData({
   accessToken: '',
   path: '/tmp',
-  name: 'storage'
+  name: 'yumonth-storage',
+  debug: true
 });
